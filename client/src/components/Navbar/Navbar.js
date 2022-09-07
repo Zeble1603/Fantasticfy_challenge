837f68f45f9c4c9dcd988f4e87f646b5dba98764
Blaise <blaisedecarvalho@gmail.com>
2022-09-07 23:53:53 +0200

    change home icon navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar() {
     return (
         <nav id='navbar'>
             <ul>
-                <li><NavLink to="/">Home</NavLink></li>
+                <li><NavLink id="home_link" to="/"><i id='home_icon' class="fa fa-home fa-lg"></i></NavLink></li>
             </ul>
         </nav>
     )
